630ee6fe0ab930cd195b0cc9e21cd22e54c26bde
Pauline VAUDON <pavaudon@e1r13p7.42.fr>
2018-08-10 14:48:50 +0200

    dans new ouverture fenetre et parser ok sans segfault

---
+++ new/includes/fdf.h
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pavaudon <lalicornede42@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/07 18:47:28 by pavaudon          #+#    #+#             */
+/*   Updated: 2018/08/07 18:47:30 by pavaudon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include "mlx.h"
+# include "libft.h"
+# include <fcntl.h>
+
+/*
+typedef struct		s_point //chaque maillon == un point a tracer
+{
+	int				z;
+	int				x;
+	int				y;
+	//int			color;
+	struct s_point	*next;
+	struct s_point	*prev;
+}					t_point;
+*/
+typedef struct		s_data
+{
+	//t_point	*point;
+	int		**tab;
+	int		x_max;
+	int		y_max;
+	char	*file;
+	int		fd_1;
+	int		fd_2;
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr;
+	int 	bpp;
+	int 	sl;
+	int 	ed;
+	char 	*data_img;
+}					t_data;
+
+int			ft_read(t_data *data);
+void		ft_new_image(t_data *data);
+int			ft_parser_line(char *line, t_data *data, int y);
+
+#endif
--- new/src/main.c
+++ new/src/main.c
@@ -5,13 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pavaudon <lalicornede42@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/07 15:47:47 by pavaudon          #+#    #+#             */
-/*   Updated: 2018/08/07 15:47:48 by pavaudon         ###   ########.fr       */
+/*   Created: 2018/08/10 13:49:09 by pavaudon          #+#    #+#             */
+/*   Updated: 2018/08/10 13:49:11 by pavaudon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-
 
 int		is_good_file(char *str)
 {
@@ -77,43 +76,30 @@ int windows_exit()
 
 int		main(int argc, char **argv)
 {
-	t_data	*data;
+	t_data *data;
 
 	if (!(data = (t_data*)ft_memalloc(sizeof(t_data))))
 		return (0);
-	//ft_struct_init(data);
 	if (argc == 2)
 	{
-		if (is_good_file(argv[1]) &&
+		if ((is_good_file(argv[1]) &&
 		((data->fd_1 = open(argv[1], O_CLOEXEC)) > 0) &&
-		((data->fd_2 = open(argv[1], O_CLOEXEC)) > 0) && ft_read(data))
+		(data->fd_2 = open(argv[1], O_CLOEXEC)) > 0) && ft_read(data))
 		{
 			data->file = argv[1];
-			write(1, "a\n", 2);
 			data->mlx_ptr = mlx_init();
-			write(1, "b\n", 2);
-			data->win_ptr = mlx_new_window(data->mlx_ptr, 1000, 1000, "tamere");
-			write(1, "c\n", 2);
-			ft_new_image(data);
-			write(1, "d\n", 2);
+			data->win_ptr = mlx_new_window(data->mlx_ptr, 1000, 1000, argv[1]);
+			//ft_new_image(data);
 			mlx_hook(data->win_ptr, 2, (1L << 01), my_press_key, 0);
-			write(1, "e\n", 2);
 			mlx_hook(data->win_ptr, 3, (1L << 01), my_remove_key, 0);
-			write(1, "f\n", 2);
 			mlx_key_hook(data->win_ptr, ft_esc, 0);
-			write(1, "g\n", 2);
 			mlx_hook(data->win_ptr, 17, 1L << 17, windows_exit, 0);
-			write(1, "h\n", 2);
 			mlx_loop(data->mlx_ptr);
-			write(1, "i\n", 2);
-			free(data->tab);
-			write(1, "j\n", 2);
 		}
 		else
 			ft_simple_printf("bad file : '%s'\n", argv[1]);
 	}
 	else
 		ft_putstr("no file\n");
-	//while (1);
 	return (0);
 }
