1b2ec87cb6dfe749e1eca60bc26b23c4d6a5383d
Pauline VAUDON <pavaudon@e1r8p5.42.fr>
2018-09-10 17:16:06 +0200

    le probleme doit venir de print pixel

--- src/bres.c
+++ src/bres.c
@@ -26,10 +26,12 @@ void line_down(t_data *data, int x, int y)
 
   i = -1;
   //color = 0xe6ea07;
-  //data->bres->error = data->bres->dx / 2;
+  printf("LINEDOWN\n");
   data->bres->error = 2 * data->bres->dy - data->bres->dx;
+  printf("dx : '%d'\tdy : '%d'\t error : '%f'\n", data->bres->dx, data->bres->dy, data->bres->error);
   inc_1 = 2 * (data->bres->dy - data->bres->dx);
   inc_2 = 2 * data->bres->dy;
+  printf("INC_1 : '%d'\tINC_2 : '%d'\n\n", inc_1, inc_2);
   while (++i < data->bres->dx)
   {
     if (data->bres->error <= 0)
@@ -40,20 +42,30 @@ void line_down(t_data *data, int x, int y)
     else
       data->bres->error += inc_2;
     x += data->bres->xincr;
-    /*
+    print_pixel(data, x, y, 0xe6ea07);
+  }
+}
+
+/*
+void line_down(t_data *data, int x, int y)
+{
+  int i;
+
+  i = -1;
+  data->bres->error = data->bres->dx / 2;
+  while (++i < data->bres->dx)
+  {
     x += data->bres->xincr;
     data->bres->error += (double)data->bres->dy;
     if (data->bres->error >= (double)data->bres->dx)
     {
       data->bres->error -= (double)data->bres->dx;
       y += data->bres->yincr;
-      //color += (i) ? 0x5103b7 : 0;
     }
-    */
     print_pixel(data, x, y, 0xe6ea07);
   }
 }
-
+*/
 void line_up(t_data *data, int x, int y)
 {
   int i;
@@ -61,10 +73,12 @@ void line_up(t_data *data, int x, int y)
   int inc_2;
 
   i = -1;
-  //data->bres->error = data->bres->dy / 2;
+  printf("LINEUP\n");
   data->bres->error = 2 * data->bres->dx - data->bres->dy;
+  printf("dx : '%d'\tdy : '%d'\t error : '%f'\n", data->bres->dx, data->bres->dy, data->bres->error);
   inc_1 = 2 * (data->bres->dx - data->bres->dy);
   inc_2 = 2 * data->bres->dx;
+  printf("INC_1 : '%d'\tINC_2 : '%d'\n\n", inc_1, inc_2);
   while (++i < data->bres->dy)
   {
     if (data->bres->error >= 0)
@@ -75,31 +89,49 @@ void line_up(t_data *data, int x, int y)
     else
       data->bres->error += inc_2;
     y += data->bres->yincr;
-    /*
-    y += data->bres->yincr;
-    data->bres->error += (double)data->bres->dx;
-    if (data->bres->error >= (double)data->bres->dy)
-    {
-      data->bres->error -= data->bres->dy;
-      x += data->bres->xincr;
-    }
-    */
     print_pixel(data, x, y, 0xe6ea07);
   }
 }
 
+/*
+void line_up(t_data *data, int x, int y)
+{
+  int i;
+
+  i = -1;
+  data->bres->error = data->bres->dy / 2;
+  while (++i < data->bres->dy)
+  {
+    y += data->bres->yincr;
+    data->bres->error += (double)data->bres->dx;
+    if (data->bres->error > (double)data->bres->dy)
+    {
+      data->bres->error -= data->bres->dy;
+      x += data->bres->xincr;
+    }
+    print_pixel(data, x, y, 0xe6ea07);
+  }
+}
+*/
 void set_var(t_data *data, int *x1, int *y1, int *x2, int *y2)
 {
+  *x1 *= data->zoom;
+  *y1 *= data->zoom;
+  *x2 *= data->zoom;
+  *y2 *= data->zoom;
+  /*
   *x1 += (*x1 * data->zoom);
   *y1 += (*y1 * data->zoom);
   *x2 += (*x2 * data->zoom);
   *y2 += (*y2 * data->zoom);
+  */
 }
 
 void ft_bres(t_data *data, int x1, int y1, int x2, int y2)
 {
-  ft_simple_printf("x1 : '%d'\ty1 : '%d'\tx2 : '%d'\ty2 : '%d'\n", x1, y1, x2, y2);
+  ft_simple_printf("\nBEFORE ZOOM\nx1 : '%d'\ty1 : '%d'\tx2 : '%d'\ty2 : '%d'\n", x1, y1, x2, y2);
   set_var(data, &x1, &y1, &x2, &y2);
+  ft_simple_printf("AFTER ZOOM\nx1 : '%d'\ty1 : '%d'\tx2 : '%d'\ty2 : '%d'\n", x1, y1, x2, y2);
   data->bres->dx = ft_abs(x2 - x1);
   data->bres->dy = ft_abs(y2 - y1);
   data->bres->xincr = (x1 < x2) ? 1 : -1;
--- src/print.c
+++ src/print.c
@@ -66,11 +66,11 @@ void draw_lines(t_data *data)
     while (++x < data->x_max[y])
     {
       if (y < data->y_max - 1)
-        //ft_bres(data, x, y - data->tab[y][x], x, (y + 1) - data->tab[y + 1][x]);
-        ft_bres(data, (x + y), y - data->tab[y][x], (x + y + 1), (y + 1) - data->tab[y + 1][x]);   // <<<< pour le decalage
+        ft_bres(data, x, y - data->tab[y][x], x, (y + 1) - data->tab[y + 1][x]);
+        //ft_bres(data, (x + y), y - data->tab[y][x], (x + y + 1), (y + 1) - data->tab[y + 1][x]);   // <<<< pour le decalage
 			if (x < data->x_max[y] - 1)
-        //ft_bres(data, x, y - data->tab[y][x], (x + 1), y - data->tab[y][x + 1]);
-        ft_bres(data, (x + y), y - data->tab[y][x], (x + y + 2), y - data->tab[y][x + 1]);         // <<<< pour le decalage
+        ft_bres(data, x, y - data->tab[y][x], (x + 1), y - data->tab[y][x + 1]);
+        //ft_bres(data, (x + y), y - data->tab[y][x], (x + y + 2), y - data->tab[y][x + 1]);         // <<<< pour le decalage
     }
   }
   mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img_ptr, 50, 50);
